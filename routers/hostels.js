bde25e4d086d3f286eb177dfb29764af45062925
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-14 12:05:55 +0100

    Add Joi and schemas for hostels and restaurants

--- routers/hostels.js
+++ routers/hostels.js
@@ -2,4 +2,63 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
+// Middleware
 app.use(express.json());
+
+app.use((_req, _res, next) => {
+  console.log("requête reçu");
+  next();
+});
+
+// JOI
+const Joi = require("joi");
+
+const hostelsSchema = Joi.object({
+  name: Joi.string().min(1).max(100).required(),
+  adress: Joi.string().required(),
+  city: Joi.string().required(),
+  country: Joi.string(),
+  stars: Joi.number().min(1).max(5).required(),
+  hasSpa: Joi.boolean().required(),
+  hasPool: Joi.boolean().required(),
+  priceCategory: Joi.number().min(1).max(3).required(),
+});
+
+// Schéma JOI :
+
+// Tableau des hôtels :
+const hostels = [
+  {
+    id: 1,
+    name: "Imperial Hotel",
+    address: "84 av des Champs-Élysées",
+    city: "Paris",
+    country: "France",
+    stars: 5,
+    hasSpa: true,
+    hasPool: true,
+    priceCategory: 3,
+  },
+  {
+    id: 2,
+    name: "The Queen",
+    address: "3 Darwin Street",
+    city: "London",
+    country: "England",
+    stars: 4,
+    hasSpa: true,
+    hasPool: false,
+    priceCategory: 3,
+  },
+  {
+    id: 3,
+    name: "Kiwi land",
+    address: "4587 George St.",
+    city: "Auckland",
+    country: "New-Zealand",
+    stars: 3,
+    hasSpa: false,
+    hasPool: true,
+    priceCategory: 2,
+  },
+];
--- routers/restaurants.js
+++ routers/restaurants.js
@@ -2,4 +2,57 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
+// Middleware
 app.use(express.json());
+
+app.use((_req, _res, next) => {
+  console.log("requête reçu");
+  next();
+});
+
+// JOI
+const Joi = require("joi");
+
+const restaurantsSchema = Joi.object({
+  name: Joi.string().min(1).max(100).required(),
+  adress: Joi.string().required(),
+  city: Joi.string().required(),
+  country: Joi.string().required(),
+  stars: Joi.number().min(1).max(5).required(),
+  cuisine: Joi.string().required(),
+  priceCategory: Joi.number().min(1).max(3).required(),
+});
+
+// Tableau des restaurants :
+const restaurants = [
+  {
+    id: 1,
+    name: "Les trois Mousquetaires",
+    address: "22 av des Champs-Élysées",
+    city: "Paris",
+    country: "France",
+    stars: 4,
+    cuisine: "french",
+    priceCategory: 3,
+  },
+  {
+    id: 2,
+    name: "The Fat Guy",
+    address: "47 Jackson Boulevard",
+    city: "New York",
+    country: "US",
+    stars: 5,
+    cuisine: "burger",
+    priceCategory: 1,
+  },
+  {
+    id: 3,
+    name: "Veggies",
+    address: "77 Avenir Street",
+    city: "Sydney",
+    country: "Australia",
+    stars: 5,
+    cuisine: "vegan",
+    priceCategory: 2,
+  },
+];
